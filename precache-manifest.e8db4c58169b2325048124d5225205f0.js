90170103edae15ff4db224d23afc722335dd2ff1
ezrabartlett <bartlettezra@gmail.com>
2023-09-10 14:38:13 -0500

    Updates

---
+++ precache-manifest.e8db4c58169b2325048124d5225205f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7779094f71134d217f65",
+    "url": "/static/css/main.9dd5fcd6.chunk.css"
+  },
+  {
+    "revision": "7779094f71134d217f65",
+    "url": "/static/js/main.14e66ff6.chunk.js"
+  },
+  {
+    "revision": "2dd2b283b8b88dbcd2e8",
+    "url": "/static/js/runtime~main.296d1d66.js"
+  },
+  {
+    "revision": "2937bf84aacc8e1ed287",
+    "url": "/static/js/2.d510fa3d.chunk.js"
+  },
+  {
+    "revision": "2f6110f28478fa8870ab",
+    "url": "/static/js/3.0c0dc788.chunk.js"
+  },
+  {
+    "revision": "f8722875071fb3f56277c4a03f7f1223",
+    "url": "/static/media/threeTone.f8722875.jpg"
+  },
+  {
+    "revision": "2591006a37d09c54a92d1c51f818c635",
+    "url": "/index.html"
+  }
+];
