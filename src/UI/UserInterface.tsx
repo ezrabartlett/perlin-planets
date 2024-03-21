ab001540178bd6908d252c4cd0b5ffd32db7023a
ezrabartlett <bartlettezra@gmail.com>
2024-03-21 01:32:50 -0500

    Controls paper

--- src/UI/UserInterface.tsx
+++ src/UI/UserInterface.tsx
@@ -14,7 +14,7 @@ export default function UserInterface(props: UserInterfaceProps) {
     const [coordinates, setCoordinates] = useState('');
 
     return (
-      <div className="absolute top-0 w-full h-full z-50 bg-transparent text-white h-400 underline pointer-events-none [&>*]:pointer-events-auto p-10">
+      <div className="absolute top-0 w-full h-full z-50 bg-transparent text-[#22577a] font-mono h-400 underline pointer-events-none [&>*]:pointer-events-auto p-10">
         <div className='float-right flex flex-col space-y-4 w-80 justify-around'>
             <div className='w-full flex flex-row space-x-2 justify-around'>
                 <input type="text" placeholder="Coordinates" value={coordinates} onChange={(e)=>(setCoordinates(e.currentTarget.value))} className="input input-bordered text-white basis-3/4" />
@@ -26,13 +26,18 @@ export default function UserInterface(props: UserInterfaceProps) {
                 <button onClick={()=>props.setCameraIndex(2)} className="btn btn-active btn-warning basis-1/3"><FaPerson className='h-[20px] w-[20px]'/></button>
             </div>
         </div>
-        <div className={`float-left flex flex-col space-y-1 w-80 text-white ${props.cameraIndex !== 2 && 'invisible'}`}>
+        <div className={`float-left flex flex-col bg-[#fdf0d5] rounded-lg p-6 space-y-1 w-80 ${props.cameraIndex !== 1 && 'invisible'}`}>
             <h1 className='text-2xl'>Controls</h1>
-            <p className="text-xl" >shift - accelerate\n</p>
-            <p className="text-xl" >w a - pitch</p>
-            <p className="text-xl" >a d - yaw</p>
-            <p className="text-xl" >e q - roll</p>
-            <p className="text-xl" >toggle speed - enter\n</p>
+            <p className="text-xl leading-9" >Accelerate - shift</p>
+            <p className="text-xl" >Pitch - w a</p>
+            <p className="text-xl" >Yaw - a d</p>
+            <p className="text-xl" >Roll - e q</p>
+            <p className="text-xl" >enter - toggle speed</p>
+        </div>
+        <div className={`float-left flex flex-col bg-[#fdf0d5] absolute rounded-lg p-6 leading-2 space-y-1 w-80 justify-items-center ${props.cameraIndex !== 2 && 'invisible'}`}>
+            <h1 className='text-2xl leading-9'>Controls</h1>
+            <p className="text-xl" >Walk - w a s d</p>
+            <p className="text-xl" >Turn - q e</p>
         </div>
       </div>
     );
